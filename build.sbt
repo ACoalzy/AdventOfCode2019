6c65e47cdedd5a5e8abde0403b5faff3883bccdb
Adam Coales <adam.coales@basementcrowd.com>
2019-12-01 09:40:14 +0000

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "aoc-2019"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
---
+++ src/main/scala/util/DayN.scala
@@ -0,0 +1,14 @@
+package util
+
+import scala.io.Source
+
+trait DayN extends App {
+
+  val num: Int
+
+  lazy val lines = Source.fromResource(s"day$num.txt").getLines.toList
+
+  def part1[A](result: A) = println(s"Part 1: $result")
+  def part2[A](result: A) = println(s"Part 2: $result")
+
+}
